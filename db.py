1951b6d56695d23dd7a93a2bf82804b83ca41022
I Man Liu <71456398+alice0217@users.noreply.github.com>
2021-04-13 16:34:12 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS parts_list (id INTEGER PRIMARY KEY, day text, hour text, minute text, zoom text)")
+        self.conn.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM parts_list")
+        rows = self.cur.fetchall()
+        return rows
+
+    def insert(self, day, hour, minute, zoom):
+        self.cur.execute("INSERT INTO parts_list VALUES (NULL, ?, ?, ?, ?)",
+                         (day, hour, minute, zoom))
+        self.conn.commit()
+
+    def remove(self, id):
+        self.cur.execute("DELETE FROM parts_list WHERE id=?", (id,))
+        self.conn.commit()
+
+    def update(self, id, day, hour, minute, zoom):
+        self.cur.execute("UPDATE parts_list SET day = ?, hour = ?, minute = ?, zoom = ? WHERE id = ?", (day, hour, minute, zoom, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
